Improved size of masked star calculation using magnitudes. Added ability to read psfMag from supplied catalogue. Added starMask to creation of pixmap for better masking. Bug fix for getstaroccluded. Bug fix for minapix. Better handling of wrong catalogue format error. Other small tidy ups, comments and refactoring.

pawlikMorphLSST/asymmetry.py
@@ -9,11 +9,12 @@ from .apertures import apercentre
 __all__ = ["minapix", "calcA"]
 
 
-def minapix(image: np.ndarray, mask: np.ndarray, apermask: np.ndarray, starMask: np.ndarray) -> List[int]:
-    """Funciton that finds the minimum asymmetry central pixel within the
+def minapix(image: np.ndarray, mask: np.ndarray, apermask: np.ndarray,
+            starMask: np.ndarray) -> List[int]:
+    """Function that finds the minimum asymmetry central pixel within the
        objects pixels of a given image.
-       Selects a range of cnadidate centroids within the brightest region that
-       comprimises of 20% of the total flux within object.
+       Selects a range of candidate centroids within the brightest region that
+       compromises of 20% of the total flux within object.
        Then measures the asymmetry of the image under rotation around that
        centroid.
        Then picks the centroid that yields the minimum A value.
@@ -30,7 +31,8 @@ def minapix(image: np.ndarray, mask: np.ndarray, apermask: np.ndarray, starMask:
     apermask : np.ndarray
         Precomputed aperture mask
     starMask : np.ndarray
-        Precomputed mask that masks stars that interfere with object measurement
+        Precomputed mask that masks stars that interfere with object
+        measurement
 
     Returns
     -------
@@ -55,7 +57,7 @@ def minapix(image: np.ndarray, mask: np.ndarray, apermask: np.ndarray, starMask:
         if pixel > 0:
             count += 1
             fluxSum += pixel
-            centroidCandidates.append([x, y])
+            centroidCandidates.append([y, x])
         if fluxSum >= twentyPercentFlux:
             break
 
@@ -67,7 +69,7 @@ def minapix(image: np.ndarray, mask: np.ndarray, apermask: np.ndarray, starMask:
         imageResidual = np.abs(image - imageRotate)
         imageResidualRavel = np.ravel(imageResidual)
 
-        regionMask = apercentre(apermask, [point[1], point[0]])
+        regionMask = apercentre(apermask, point)
         regionIndicies = np.nonzero(np.ravel(regionMask) == 1)[0]
         region = imageRavel[regionIndicies]
         regionResidual = imageResidualRavel[regionIndicies]
@@ -128,7 +130,7 @@ def calcA(img: np.ndarray, pixmap: np.ndarray, apermask: np.ndarray,
     # rotate the star mask angle degrees, so that star does not interfere
     # with measurement
     starMask *= transform.rotate(starMask, angle, center=(cenpix_x, cenpix_y),
-                              preserve_range=True, cval=1.)
+                                 preserve_range=True, cval=1.)
 
     # mask image
     img *= starMask

pawlikMorphLSST/diagnostic.py
@@ -30,7 +30,16 @@ def _getStarsOccludObject(file, header):
     names, RAs, DECs, *rest = np.loadtxt(starcat, unpack=True, skiprows=1,
                                          delimiter=",", dtype=str)
 
-    names = list(names)
+    # If there is only one entry in csv file then loadtxt returns str for each
+    # variable. Thefore we check if length of names and RAs match, if they dont
+    # match put str in a list else just turn names into a list as other should
+    # already be a list
+    if len(RAs) != len(names):
+        names = [names]
+        RAs = [RAs]
+        DECs = [DECs]
+    else:
+        names = list(names)
     occludingStars = []
 
     try:
@@ -66,39 +75,40 @@ def make_figure(result):
     mask = fits.getdata(filemask)
 
     listofStarstoPlot = _getStarsOccludObject(file, header)
+    if len(listofStarstoPlot) > 0:
+        fig, axs = plt.subplots(1, 2)
+        fig.set_figheight(11.25)
+        fig.set_figwidth(20)
 
-    fig, axs = plt.subplots(1, 2)
-    fig.set_figheight(11.25)
-    fig.set_figwidth(20)
+        axs[0].imshow(log_stretch(_normalise(img)), origin="lower", aspect="auto")
+        axs[0].scatter(result.apix[0], result.apix[1], label="Asym. centre")
+        if(len(listofStarstoPlot) > 0):
+            axs[0].scatter(*zip(*listofStarstoPlot), label="STAR")
 
-    axs[0].imshow(log_stretch(_normalise(img)), origin="lower", aspect="auto")
-    axs[0].scatter(result.apix[1], result.apix[0], label="Asym. centre")
-    if(len(listofStarstoPlot) > 0):
-        axs[0].scatter(*zip(*listofStarstoPlot), label="STAR")
+        text = f"Sky={result.sky:.2f}\n" fr"Sky $\sigma$={result.sky_err:.2f}"
 
-    text = f"Sky={result.sky:.2f}\n" fr"Sky $\sigma$={result.sky_err:.2f}"
+        axs[0].text(2, 7, text,
+                    horizontalalignment='left', verticalalignment='top',
+                    bbox=dict(facecolor='white', alpha=1.0, boxstyle='round'))
 
-    axs[0].text(2, 7, text,
-                horizontalalignment='left', verticalalignment='top',
-                bbox=dict(facecolor='white', alpha=1.0, boxstyle='round'))
+        _supressAxs(axs[0])
 
-    _supressAxs(axs[0])
+        axs[1].imshow(mask, origin="lower", aspect="auto", cmap="gray")
+        axs[1].scatter(result.apix[0], result.apix[1], label="Asym. centre")
+        if(len(listofStarstoPlot) > 0):
+            axs[1].scatter(*zip(*listofStarstoPlot), label="STAR")
 
-    axs[1].imshow(mask, origin="lower", aspect="auto", cmap="gray")
-    axs[1].scatter(result.apix[1], result.apix[0], label="Asym. centre")
-    if(len(listofStarstoPlot) > 0):
-        axs[1].scatter(*zip(*listofStarstoPlot), label="STAR")
+        text = f"A={result.A[0]:.3f}\nA_bgr={result.A[1]:.3f}\n" rf"$A_s$={result.As[0]:.3f}"
+        text += "\n" fr"$A_s90$={result.As90[0]:.3f}"
 
-    text = f"A={result.A[0]:.3f}\nA_bgr={result.A[1]:.3f}\n" rf"$A_s$={result.As[0]:.3f}"
-    text += "\n" fr"$A_s90$={result.As90[0]:.3f}"
+        axs[1].text(2, 13, text,
+                    horizontalalignment='left', verticalalignment='top',
+                    bbox=dict(facecolor='white', alpha=1.0, boxstyle='round'))
+        circle = mpatches.Circle(result.apix, result.rmax, fill=False, label="Rmax", color="white")
+        axs[1].add_patch(circle)
+        _supressAxs(axs[1])
 
-    axs[1].text(2, 13, text,
-                horizontalalignment='left', verticalalignment='top',
-                bbox=dict(facecolor='white', alpha=1.0, boxstyle='round'))
-    circle = mpatches.Circle(result.apix, result.rmax, fill=False, label="Rmax", color="white")
-    axs[1].add_patch(circle)
-    _supressAxs(axs[1])
-
-    plt.subplots_adjust(top=0.985, bottom=0.015, left=0.008, right=0.992, hspace=0.2, wspace=0.016)
-    plt.legend()
-    plt.show()
+        plt.subplots_adjust(top=0.985, bottom=0.015, left=0.008, right=0.992, hspace=0.2, wspace=0.016)
+        plt.legend()
+        # plt.savefig("result_" + file[20:-9] + "_old.png", dpi=96)
+        plt.show()

pawlikMorphLSST/imageutils.py
@@ -431,14 +431,22 @@ def circle_mask(shape, centre, radius):
     return circmask*anglemask
 
 
-def calcR(counts, psf, sigma):
+def calcR(psf, counts, sigma):
     return sigma * np.sqrt(2. * np.log(counts / psf))
 
 
-def maskstarsPSF(img, objs, header, sky):
+def maskstarsPSF(img, objs, header, skyCount):
 
     s1 = header["PSF_S1"]
     s2 = header["PSF_S2"]
+    exptime = header["EXPTIME"]
+    aa = header["PHT_AA"]
+    kk = header["PHT_KK"]
+    airmass = header["AIRMASS"]
+    skyCount = header["SKY"]
+
+    fact = 0.4*(aa + kk*airmass)
+
     try:
         bzero = header["BZERO"]
     except KeyError:
@@ -458,18 +466,24 @@ def maskstarsPSF(img, objs, header, sky):
         x = round(float(x))
         y = round(float(y))
 
-        objCount = np.amax(img[y-1:y+1, x-1:x+1])
-        sigma = max(s1, s2)
+        # objectCount = np.amax(img[y-1:y+1, x-1:x+1])
+        # objectFluxRatio = (objectCount / exptime) * 10**(fact)
+        objectMag = obj[3]#-2.5 * np.log10(objectFluxRatio)
 
-        radius = calcR(objCount, sky - bzero, sigma)
-        aps = CircularAperture(pixelPos, r=2.4*radius)
+        skyFluxRatio = ((skyCount - bzero) / exptime) * 10**(fact)
+        skyMag = -2.5 * np.log10(skyFluxRatio)
+
+        sigma = max(s1, s2)
+        radius = calcR(objectMag, skyMag, sigma)
+
+        aps = CircularAperture(pixelPos, r=5*radius)
         aperMask = np.zeros_like(img)
         masks = aps.to_mask(method="subpixel")
         aperMask = np.where(masks.to_image(img.shape) > 0., 1., 0.)
-        aperMask = ndimage.morphology.binary_dilation(aperMask, border_value=0, iterations=3)
+        # aperMask = ndimage.morphology.binary_dilation(aperMask, border_value=0, iterations=3)
 
-        newMask = circle_mask(mask.shape, (pixelPos[1], pixelPos[0]), 2.4*radius)
-        newMask = ndimage.morphology.binary_dilation(newMask, border_value=0, iterations=3)
+        newMask = circle_mask(mask.shape, (pixelPos[1], pixelPos[0]), 5.*radius)
+        # newMask = ndimage.morphology.binary_dilation(newMask, border_value=0, iterations=3)
         mask = np.logical_or(mask, newMask)
         mask = np.logical_or(mask, aperMask)
 

pawlikMorphLSST/pixmap.py
@@ -7,51 +7,59 @@ from skimage import transform
 __all__ = ["pixelmap"]
 
 
-def pixelmap(img: np.ndarray, thres: float, filtsize: int) -> np.ndarray:
-    # from matplotlib import animation
+def pixelmap(image: np.ndarray, threshold: float, filterSize: int,
+             starMask=None) -> np.ndarray:
     ''' Calculates an object binary mask using a mean filter and 8 connected
         pixels and a given threshold.
 
     Parameters
     ----------
 
-    img : np.ndarray
+    image : np.ndarray
         Image from which the binary mask is calculated.
-    thres : float
+    threshold : float
         Threshold for calculating 8 connectedness
-    filtsize : int
+    filterSize : int
         Size of the mean filter. Must be odd
+    starMask : optional, None or np.ndarray
+        Mask that mask out nuisance stars
 
     Returns
     -------
-    objmask : np.ndrray
+    objectMask : np.ndrray
         Calculated binary object mask
 
     '''
 
-    if filtsize % 2 == 0:
+    if starMask is None:
+        starMask = np.full(image.shape, True)
+    imageTmp = image * starMask
+
+    if filterSize % 2 == 0:
         print("ERROR! Filter can not be of even size.")
         sys.exit()
 
-    imgsize = img.shape[0]
+    imgsize = imageTmp.shape[0]
 
     # mean filter
-    img = ndimage.uniform_filter(img, size=filtsize, mode="reflect")
+    imageTmp = ndimage.uniform_filter(imageTmp, size=filterSize,
+                                      mode="reflect")
     # resize image to match PawlikMorph
     # TODO leave this as an option?
-    img = transform.resize(img, (int(imgsize / filtsize), int(imgsize / filtsize)),
-                           anti_aliasing=False, preserve_range=True)
+    imageTmp = transform.resize(imageTmp, (int(imgsize / filterSize),
+                                int(imgsize / filterSize)),
+                                anti_aliasing=False, preserve_range=True)
 
-    npix = img.shape[0]
+    npix = imageTmp.shape[0]
     cenpix = np.array([int(npix/2), int(npix/2)])
-    if img[cenpix[0], cenpix[1]] < thres:
+    if imageTmp[cenpix[0], cenpix[1]] < threshold:
         print("ERROR! Central pixel too faint")
         # sys.exit()
 
     # output binary image array
-    objmask = np.zeros_like(img)
+    objectMask = np.zeros_like(imageTmp)
     # set central pixel as this is always included
-    objmask[cenpix[0], cenpix[1]] = 1
+    objectMask[cenpix[0], cenpix[1]] = 1
 
     # start list with central pixel
     pixels = [cenpix]
@@ -72,13 +80,14 @@ def pixelmap(img: np.ndarray, thres: float, filtsize: int) -> np.ndarray:
             ycur += yvec[i]
             if xcur >= npix or ycur >= npix or xcur < 0 or ycur < 0:
                 continue
-            if img[xcur, ycur] > thres and objmask[xcur, ycur] == 0:
-                objmask[xcur, ycur] = 1
+            if imageTmp[xcur, ycur] > threshold and objectMask[xcur, ycur] == 0:
+                objectMask[xcur, ycur] = 1
                 pixels.append([xcur, ycur])
         if len(pixels) == 0:
             pixelsleft = False
             break
 
     # resize binary image to original size
-    objmask = transform.resize(objmask, (imgsize, imgsize), order=0, mode="edge")
-    return objmask
+    objectMask = transform.resize(objectMask, (imgsize, imgsize), order=0,
+                                  mode="edge")
+    return objectMask