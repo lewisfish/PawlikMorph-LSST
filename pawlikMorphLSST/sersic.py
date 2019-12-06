8cea68debc431467dad97dc3133a371d49a43a82
Lewis McMillan <lewistmcmillan@gmail.com>
2019-12-06 12:01:49 +0000

    Update docstring

--- pawlikMorphLSST/sersic.py
+++ pawlikMorphLSST/sersic.py
@@ -7,21 +7,26 @@ __all__ = ["fitSersic"]
 
 
 def fitSersic(image: np.ndarray, centroid: List[float], fwhms: List[float], theta: float):
-    '''
+    '''Function that fits a 2D sersic function to an image.
 
     Parameters
     ----------
 
     image : np.ndarray
-
+        image to which a 2D Sersic function will be fit
     centroid : List[float]
-
+        Centre of object of interest
     fwhms : List[float]
-
+        Full width half maximums of object
     theta : float
+        rotation of object anticlockwise from positive x axis
 
     Returns
     -------
+
+    Parameters : astropy.modeling.Model object
+
+        Collection of best fir parameters for the 2D sersic function
 
     '''
 
@@ -30,7 +35,6 @@ def fitSersic(image: np.ndarray, centroid: List[float], fwhms: List[float], thet
     # amplitude => Surface brightness at r_eff
     # r_eff => Effective (half-light) radius
     # n => sersic index, guess 1.?
-    # theta, rotation angle in radians, counterclockwise from +ive x axis
 
     ellip = (max(fwhms) - min(fwhms)) / max(fwhms)
 
@@ -39,6 +43,6 @@ def fitSersic(image: np.ndarray, centroid: List[float], fwhms: List[float], thet
     ny, nx = image.shape
     y, x = np.mgrid[0:ny, 0:nx]
 
-    p = fit_p(sersic_init, x, y, image, maxiter=10000)
+    Parameters = fit_p(sersic_init, x, y, image, maxiter=500, acc=1e-5)
 
-    return p
+    return Parameters
