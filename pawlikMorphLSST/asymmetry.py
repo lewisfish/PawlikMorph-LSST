Added cmdline argument to select number of cores to use. Fixed unintended side effect in calcA. Tidied up output of diagnostic.py. Added star mask pixel-fraction calculation. Added star mask to sersic calculation

pawlikMorphLSST/asymmetry.py
@@ -66,18 +66,17 @@ def minapix(image: np.ndarray, mask: np.ndarray, apermask: np.ndarray,
         if fluxSum >= twentyPercentFlux:
             break
 
-    imageRavel = np.ravel(image)
     a = np.zeros(count)
 
     # test centroid candidates for minima of asymmetry
     for i, point in enumerate(centroidCandidates):
-        imageRotate = transform.rotate(image, 180., center=point, preserve_range=True)
-        imageResidual = np.abs(image - imageRotate)
+        imageRotate = transform.rotate(imageMask, 180., center=point, preserve_range=True)
+        imageResidual = np.abs(imageMask - imageRotate)
         imageResidualRavel = np.ravel(imageResidual)
 
         regionMask = apercentre(apermask, point)
         regionIndicies = np.nonzero(np.ravel(regionMask) == 1)[0]
-        region = imageRavel[regionIndicies]
+        region = imageMaskRavel[regionIndicies]
         regionResidual = imageResidualRavel[regionIndicies]
 
         regionMask *= 0
@@ -115,7 +114,8 @@ def calcA(img: np.ndarray, pixmap: np.ndarray, apermask: np.ndarray,
         Angle to rotate object, in degrees.
 
     starMask : np.ndarray
-        Precomputed mask that masks stars that interfere with object measurement
+        Precomputed mask that masks stars that interfere with object
+        measurement
 
     noisecorrect : bool, optional
         Default value False. If true corrects for background noise
@@ -132,19 +132,19 @@ def calcA(img: np.ndarray, pixmap: np.ndarray, apermask: np.ndarray,
     cenpix_y = centroid[1]
 
     # cast to float so that rotate is happy
-    starMask = starMask.astype(np.float64)
+    starMaskCopy = starMask.astype(np.float64)
     # rotate the star mask angle degrees, so that star does not interfere
     # with measurement
-    starMask *= transform.rotate(starMask, angle, center=(cenpix_x, cenpix_y),
+    starMaskCopy *= transform.rotate(starMaskCopy, angle, center=(cenpix_x, cenpix_y),
                                  preserve_range=True, cval=1.)
 
     # mask image
-    img *= starMask
-    imgRot = transform.rotate(img, angle, center=(cenpix_x, cenpix_y),
+    imgCopy = img * starMaskCopy
+    imgRot = transform.rotate(imgCopy, angle, center=(cenpix_x, cenpix_y),
                               preserve_range=True)
 
-    imgResid = np.abs(img - imgRot)
-    imgravel = np.ravel(img)
+    imgResid = np.abs(imgCopy - imgRot)
+    imgravel = np.ravel(imgCopy)
 
     netmask = apermask
 
@@ -159,12 +159,12 @@ def calcA(img: np.ndarray, pixmap: np.ndarray, apermask: np.ndarray,
 
         # build "background noise" image using morphological dilation
         # https://en.wikipedia.org/wiki/Dilation_(morphology)
-        bgrimg = np.zeros_like(img)
+        bgrimg = np.zeros_like(imgCopy)
         bgrimg = np.ravel(bgrimg)
         element = np.ones((9, 9))
 
         # mask pixel map
-        pixmap *= starMask
+        pixmap *= starMaskCopy
         mask = ndimage.morphology.binary_dilation(pixmap, structure=element)
         maskind = np.nonzero(np.ravel(mask) == 1)[0]
         bgrind = np.nonzero(np.ravel(mask) != 1)[0]
@@ -189,7 +189,7 @@ def calcA(img: np.ndarray, pixmap: np.ndarray, apermask: np.ndarray,
                 bgrimg[bgrind] = bgrpix
                 bgrimg[maskind] = maskpix
 
-                bgrimg = bgrimg.reshape((img.shape[0], img.shape[0]))
+                bgrimg = bgrimg.reshape((imgCopy.shape[0], imgCopy.shape[0]))
                 bgrimgRot = transform.rotate(bgrimg, 180., center=(cenpix_y, cenpix_x), preserve_range=True)
                 bgrimgResid = np.ravel(np.abs(bgrimg - bgrimgRot))
 

pawlikMorphLSST/pixmap.py
@@ -1,10 +1,44 @@
-import sys
+from typing import List
 
 import numpy as np
 from scipy import ndimage
 from skimage import transform
 
-__all__ = ["pixelmap"]
+__all__ = ["pixelmap", "calcMaskedFraction"]
+
+
+def calcMaskedFraction(oldMask: np.ndarray, starMask: np.ndarray,
+                       cenpix: List[float]) -> float:
+    '''Function that calculates the fraction of object pixels that are masked
+       due to presence of a star
+
+    Parameters
+    ----------
+
+    oldMask : np.ndarray
+        Mask containing object of interest.
+
+    starMask : np.ndarray
+        Mask containing location of star
+
+    cenpix : List[float]
+        Centre of asymmetry in pixels.
+
+    Returns
+    -------
+
+    Fraction of object pixels masked by stars : float
+
+    '''
+
+    starMaskCopy = starMask * transform.rotate(starMask, 180.,
+                                               center=(cenpix[0], cenpix[1]),
+                                               preserve_range=True, cval=1.)
+
+    maskedFraction = np.sum(oldMask * starMaskCopy)
+    objectFraction = np.sum(oldMask)
+
+    return 1. - (maskedFraction / objectFraction)
 
 
 def pixelmap(image: np.ndarray, threshold: float, filterSize: int,

pawlikMorphLSST/sersic.py
@@ -6,7 +6,8 @@ from astropy.modeling import models, fitting
 __all__ = ["fitSersic"]
 
 
-def fitSersic(image: np.ndarray, centroid: List[float], fwhms: List[float], theta: float):
+def fitSersic(image: np.ndarray, centroid: List[float], fwhms: List[float],
+              theta: float, starMask: np.ndarray):
     '''Function that fits a 2D sersic function to an image of a Galaxy.
 
     Parameters
@@ -20,6 +21,8 @@ def fitSersic(image: np.ndarray, centroid: List[float], fwhms: List[float], thet
         Full width half maximums of object
     theta : float
         rotation of object anticlockwise from positive x axis
+    starMask : np.ndarray
+        Mask contains star locations.
 
     Returns
     -------
@@ -30,6 +33,7 @@ def fitSersic(image: np.ndarray, centroid: List[float], fwhms: List[float], thet
 
     '''
 
+    imageCopy = image * starMask
     fit_p = fitting.LevMarLSQFitter()
 
     # amplitude => Surface brightness at r_eff
@@ -39,11 +43,14 @@ def fitSersic(image: np.ndarray, centroid: List[float], fwhms: List[float], thet
     ellip = (max(fwhms) - min(fwhms)) / max(fwhms)
 
     # TODO better guess of intial values?
-    sersic_init = models.Sersic2D(amplitude=np.mean(image), r_eff=max(fwhms), n=1.5, x_0=centroid[0], y_0=centroid[1],
+    sersic_init = models.Sersic2D(amplitude=np.mean(imageCopy),
+                                  r_eff=max(fwhms), n=1.5,
+                                  x_0=centroid[0], y_0=centroid[1],
                                   ellip=ellip, theta=theta)
-    ny, nx = image.shape
+
+    ny, nx = imageCopy.shape
     y, x = np.mgrid[0:ny, 0:nx]
 
-    Parameters = fit_p(sersic_init, x, y, image, maxiter=500, acc=1e-5)
+    Parameters = fit_p(sersic_init, x, y, imageCopy, maxiter=500, acc=1e-5)
 
     return Parameters