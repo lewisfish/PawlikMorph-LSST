Fixed bug in calculation of Rmax, as was not using brightest pixel, but rather the central image pixel...

pawlikMorphLSST/apertures.py
@@ -140,14 +140,14 @@ def distarr(npixx: int, npixy: int, cenpix: np.ndarray) -> np.ndarray:
 
     '''
 
-    x1 = np.arange(npixx) - cenpix[0]
-    x2 = np.ones_like(x1)
-
     y1 = np.arange(npixy) - cenpix[1]
     y2 = np.ones_like(y1)
 
-    pixx = np.transpose(np.outer(x1, y2))
-    pixy = np.transpose(np.outer(x2, y1))
+    x1 = np.arange(npixx) - cenpix[0]
+    x2 = np.ones_like(x1)
+
+    pixy = np.transpose(np.outer(y1, x2))
+    pixx = np.transpose(np.outer(y2, x1))
 
     dist = np.sqrt(pixx**2 + pixy**2)
 

pawlikMorphLSST/pixmap.py
@@ -23,7 +23,7 @@ class _ImageError(_Error):
         raise AttributeError
 
 
-def calcRmax(mask):
+def calcRmax(image, mask):
     '''Function to calculate the maximum extent of a binary pixel map
 
     Parameters
@@ -40,11 +40,10 @@ def calcRmax(mask):
 
     '''
 
-    imgsize = mask.shape[0]
     objectpix = np.nonzero(mask == 1)
-    cenpix = np.array([int(imgsize/2) + 1, int(imgsize/2) + 1])
+    cenpix = np.array(np.unravel_index(image.argmax(), image.shape))
 
-    distarray = distarr(imgsize, imgsize, cenpix)
+    distarray = distarr(image.shape[0], image.shape[1], cenpix)
     objectdist = distarray[objectpix]
     rmax = np.max(objectdist)
 