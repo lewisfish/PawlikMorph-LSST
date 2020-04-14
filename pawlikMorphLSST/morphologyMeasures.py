Fixed smoothness/clumpiness calculation. Now follows that of usual method plus M. Pawliks method.

pawlikMorphLSST/morphologyMeasures.py
@@ -162,33 +162,87 @@ def m20(image, pixelmap):
 
 
 def smoothness(image, mask, centroid, Rmax, r20, sky):
+    '''Calculate the smoothness of clumpiness of the galaxy of interest.
+
+    Parameters
+    ----------
+
+    image : float, 2d np.ndarray
+        Image of galaxy
+
+    mask : float [0. - 1.], 2d np.ndarray
+        Mask which contains the pixels belonging to the galaxy of interest.
+
+    centroid : List[float]
+        Pixel location of the brightest pixel in galaxy.
+
+    Rmax : float
+        Distance from brightest pixel to furthest pixel in galaxy
+
+    r20 : float
+        Distance from brightest pixel to radius at which 20% of light of galaxy
+        is enclosed.
+
+    sky : float
+        Value of the sky background.
+
+    Returns
+    -------
+
+    Result: float
+        The smoothness or clumpiness parameter, S.
+
+    '''
 
     r_in = r20
     r_out = Rmax
 
+    # Exclude inner 20% of light. See Concelice 2003
     imageApeture = CircularAnnulus(centroid, r_in, r_out)
 
+    # smooth image
     imageSmooth = ndi.uniform_filter(image, size=int(r20))
 
+    # calculate residual, setting any negative pixels to 0.
     imageDiff = image - imageSmooth
     imageDiff[imageDiff < 0.] = 0.
 
+    # calculate S, accounting for the background smoothness.
     imageFlux = imageApeture.do_photometry(image, method="exact")[0][0]
     diffFlux = imageApeture.do_photometry(imageDiff, method="exact")[0][0]
-    bgr = getBackground(image, mask, sky)
-    # backgroundSmooth = 
-
-    result = (diffFlux - backgroundSmooth) / imageFlux
+    backgroundSmooth = getBackground(image, mask, sky, r20)
+    result = (diffFlux - imageApeture.area*backgroundSmooth) / imageFlux
 
     return result
 
 
-def getBackground(image, mask, sky):
+def getBackground(image, mask, sky, boxcarSize):
+    '''Calculate the background smoothness.
 
-    # start at 32 then half if not found
-    # check box does not overlap object
-    # slide right/up 2 pixels at a time
+    Parameters
+    ----------
 
+    image : float, 2d np.ndarray
+        The image.
+
+    mask : float [0-1], 2d np.ndarray
+        mask that contains the galaxy objects pixels
+
+    sky : float
+        Value of the sky background
+
+    boxcarSize: float or int
+        Size of the box to use in median filter
+
+    Returns
+    -------
+
+    The smoothness of the background. Float
+
+    '''
+
+    # Set the galxy pixels to -99 so that we dont include them in background
+    # smoothness calculation
     maskCopy = ~np.array(mask, dtype=np.bool)
     imageCopy = image * maskCopy
     imageCopy[imageCopy == 0.] = -99
@@ -198,6 +252,10 @@ def getBackground(image, mask, sky):
 
     bgr = -99.
 
+    # move box of size boxSize over image till we find an area that is only background.
+    # start at boxSize=32 then half if not found
+    # check box does not overlap galaxy object
+    # slide right/up 2 pixels at a time if not found
     while True:
         skyPixels = imageCopy[posY:posY+boxSize, posX:posX+boxSize]
         boxMean = np.sum(skyPixels) / (boxSize**2)
@@ -214,8 +272,15 @@ def getBackground(image, mask, sky):
                 if boxSize > 8:
                     posY = 0
                     posX = 0
-                    boxSize = int(boxSize / 2)
+                    boxSize //= 2
                 else:
                     bgr = -99.
                     break
-    return bgr
+
+    # Calculate smoothness
+    bkg = image[posY:posY+boxSize, posX:posX+boxSize]
+    bkgSmooth = ndi.uniform_filter(bkg, size=int(boxcarSize))
+    bkgDiff = bkg - bkgSmooth
+    bkgDiff[bkgDiff < 0.] = 0.
+
+    return np.sum(bkgDiff) / float(bkg.size)