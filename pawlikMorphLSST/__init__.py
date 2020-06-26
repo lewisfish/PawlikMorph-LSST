ec0e345c21fb590a42694e56563cd4e1126e3a05
lewis <lewistmcmillan@gmail.com>
2020-06-26 10:55:26 +0100

    change __init__.py

--- pawlikMorphLSST/__init__.py
+++ pawlikMorphLSST/__init__.py
@@ -2,6 +2,22 @@
     morphological properties.
 '''
 
-__all__ = ["gaussfitter", "pixmap", "apertures", "asymmetry", "engines",
-           "imageutils", "objectMasker", "sersic", "result", "main",
-           "casgm", "helpers", "skyBackground", "image", "diagnostic"]
+# __all__ = ["gaussfitter", "pixmap", "apertures", "asymmetry", "engines",
+#            "imageutils", "objectMasker", "sersic", "result", "main",
+#            "casgm", "helpers", "skyBackground", "image", "diagnostic"]
+
+from .gaussfitter import *
+from .pixmap import *
+from .apertures import *
+from .asymmetry import *
+from .engines import *
+from .imageutils import *
+from .objectMasker import *
+from .sersic import *
+from .result import *
+from .main import *
+from .casgm import *
+from .helpers import *
+from .skyBackground import *
+from .image import *
+from .diagnostic import *
