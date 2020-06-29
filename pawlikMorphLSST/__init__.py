a72465be202862ba4b9387ab99d8ebe4564437c7
lewis <lewistmcmillan@gmail.com>
2020-06-29 11:02:19 +0100

    Changed name of image module so that it wont;t clash with user defined variables image. Changed __init__.py so that it only uses __all__ now.

--- pawlikMorphLSST/__init__.py
+++ pawlikMorphLSST/__init__.py
@@ -2,22 +2,6 @@
     morphological properties.
 '''
 
-# __all__ = ["gaussfitter", "pixmap", "apertures", "asymmetry", "engines",
-#            "imageutils", "objectMasker", "sersic", "result", "main",
-#            "casgm", "helpers", "skyBackground", "image", "diagnostic"]
-
-from .gaussfitter import *
-from .pixmap import *
-from .apertures import *
-from .asymmetry import *
-from .engines import *
-from .imageutils import *
-from .objectMasker import *
-from .sersic import *
-from .result import *
-from .main import *
-from .casgm import *
-from .helpers import *
-from .skyBackground import *
-from .image import *
-from .diagnostic import *
+__all__ = ["gaussfitter", "pixmap", "apertures", "asymmetry", "engines",
+           "imageutils", "objectMasker", "sersic", "result", "main",
+           "casgm", "helpers", "skyBackground", "Image", "diagnostic"]
