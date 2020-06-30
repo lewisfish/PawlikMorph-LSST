faf57dfd942108323a6c913a1531f9ba05c50558
lewis <lewistmcmillan@gmail.com>
2020-06-30 16:15:06 +0100

    Added more docs on result.py

--- docs/result.rst
+++ docs/result.rst
@@ -2,8 +2,11 @@
 Result
 ******
 
+This module contains the `data <https://docs.python.org/3/library/dataclasses.html>`_ class which acts as a container for all the results and parameters calculated or needed for the analysis code.
+Default value for numerical values is -99. If in the final results -99 appears this means that the value was either not clacluated or there has been an error with that part of the analysis.
+Default value for strings is an empty string.
 
-.. automodule:: pawlikMorphLSST.result
+.. autoclass:: pawlikMorphLSST.result.Result
     :members:
     :undoc-members:
     :noindex:
--- pawlikMorphLSST/result.py
+++ pawlikMorphLSST/result.py
@@ -6,46 +6,73 @@ __all__ = ["Result"]
 
 @dataclass
 class Result:
-    '''Class that stores the results of image analysis'''
+    '''Data class that stores the results of image analysis.'''
 
-    # names of files and folders used or generated in analysis
+    #: Filename of image
     file: str
+    #: Output folder for saving data
     outfolder: Any
+    #: Filename of output data for objects that occlude with segmentation map.
     occludedFile: str
+    #: Path to segmentation map
     pixelMapFile: Any = ""
+    #: path to clean image.
     cleanImage: Any = ""
+    #: path to star mask
     starMask: Any = ""
+    #: List of objects RA, DECs that occlude objects segmentation map.
     objList: Any = field(default_factory=lambda: [])
-    # Calculated asymmetry values
+    #: Calculated asymmetry value, format [A, A_error]
     A: List[float] = field(default_factory=lambda: [-99., -99.])
+    #: Calculated shape asymmetry value, format [As, As_error]
     As: List[float] = field(default_factory=lambda: [-99., -99.])
+    #: Calculated shape asymmetry 90 value, format [As90, As90_error]
     As90: List[float] = field(default_factory=lambda: [-99., -99.])
-    # Misc calculated values
+    #: Maxmimum radius of the segmentation map
     rmax: float = -99
+    #: Asymmetry (A) minimised central pixel
     apix: Tuple[float] = (-99., -99.)
+    #: Sky background value.
     sky: float = -99.
+    #: Sky background error.
     sky_err: float = 99.
+    #: FWHM's of the fitted 2D Gaussian
     fwhms: List[float] = field(default_factory=lambda: [-99., -99.])
+    #: Theta of the fitted 2D Gaussian
     theta: float = -99.
-    # CAS
+    #: Radius in which 20% of total light flux is contained
     r20: float = -99.
+    #: Radius in which 80% of total light flux is contained
     r80: float = -99.
-    C: float = -99.    # Concentration
+    #: Concentraion value
+    C: float = -99.
+    #: Gini index
     gini: float = -99.
+    #: M20 value
     m20: float = -99.
-    S: float = -99.    # clumpiness/smoothness
-    # Sersic fit values
+    #: Smoothness value.
+    S: float = -99.
+    #: Sersic amplitude.
     sersic_amplitude: float = -99.
+    #: Sersic effective radius
     sersic_r_eff: float = -99.
+    #: Sersic index.
     sersic_n: float = -99.
+    #: Sersic x centre
     sersic_x_0: float = -99.
+    #: Sersic y centre
     sersic_y_0: float = -99.
+    #: Sersic ellipticity.
     sersic_ellip: float = -99.
+    #: Sersic rotation.
     sersic_theta: float = -99.
+    #: Time taken to analyse image.
     time: float = 0.
-    # Flags that signify data may not be useful
+    #: If true means that there is a star in the catalogue occluding the objects segmentation map
     star_flag: bool = False
+    #: Fraction of pixels masked due to occluding star/object.
     maskedPixelFraction: float = -99.
+    #: If true then the segmentation map extends to an edge of the image.
     objectEdge: bool = False
 
     def write(self, objectfile):
